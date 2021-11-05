a4069753f16884935900c0e1d8aa6f9281baead4
momo <mouiad@kuwaitnet.dev>
2021-11-05 10:17:03 +0200

    Add Expense model to the admin

--- backend/expensia/expense/admin.py
+++ backend/expensia/expense/admin.py
@@ -1,5 +1,28 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Expense
 
 # Register your models here.
 admin.site.register(Category)
+
+
+class ExpenseAdmin(admin.ModelAdmin):
+
+    model = Expense
+    date_hierarchy = 'start_date'
+    list_display = ('name', 'amount', 'start_date', 'end_date',)
+    list_filter = ('start_date', 'end_date', 'category')
+    filter_horizontal = ('category',)
+    order = ['-start_date']
+    fieldsets = (
+        (None, {'fields': (
+            'name', 'amount', 'start_date', 'end_date', 'category',)}),
+    )
+
+    def has_add_permission(self, request, obj=None):
+        return True
+
+    def get_list_display(self, request):
+        return self.list_display
+
+
+admin.site.register(Expense, ExpenseAdmin)
