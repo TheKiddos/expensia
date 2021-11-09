d7bca0216704e5c05b157c6b6a663266e1e3ab3a
Zahlt <ahmadzahloote@gmail.com>
2021-11-09 21:34:17 +0200

    Update Settings (fixes#14)

--- backend/expensia/expensia/settings.py
+++ backend/expensia/expensia/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -127,6 +127,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+
+MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- backend/expensia/expensia/urls.py
+++ backend/expensia/expensia/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/expense/', include('expense.urls', namespace='expense'),)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
