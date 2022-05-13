52e2431d28ab5775282e241ee8711196e0691427
J2mF <mp4-12cs5@hotmail.com>
2022-05-13 10:51:55 +0300

    [WIP] Implement Latest Spending (#21)
    Change Log:
    - Add ExpenseList Title

--- frontend/expensia/app/scripts/views/components/expense/ExpenseList.vue
+++ frontend/expensia/app/scripts/views/components/expense/ExpenseList.vue
@@ -1,20 +1,23 @@
 <template>
-  <ui-list :type="2">
-    <ui-item v-for="expense in expenses" :key="expense.id">
-      <ui-item-first-content>
-        <ui-icon>attach_money</ui-icon>
-        {{ expense.amount }}
-      </ui-item-first-content>
+  <ui-list-group>
+    <ui-list-group-subheader class="list__title">Latest Spending</ui-list-group-subheader>
+    <ui-list :type="2">
+      <ui-item v-for="expense in expenses" :key="expense.id">
+        <ui-item-first-content>
+          <ui-icon>attach_money</ui-icon>
+          {{ expense.amount }}
+        </ui-item-first-content>
 
-      <ui-item-text-content>
-        <ui-item-text1>{{ expense.name }}</ui-item-text1>
-        <ui-item-text2>{{ expense.category.name }}</ui-item-text2>
-      </ui-item-text-content>
-      <ui-item-last-content>
-        <ui-icon-button icon="density_small"></ui-icon-button>
-      </ui-item-last-content>
-    </ui-item>
-  </ui-list>
+        <ui-item-text-content>
+          <ui-item-text1>{{ expense.name }}</ui-item-text1>
+          <ui-item-text2>{{ expense.category.name }}</ui-item-text2>
+        </ui-item-text-content>
+        <ui-item-last-content>
+          <ui-icon-button icon="density_small"></ui-icon-button>
+        </ui-item-last-content>
+      </ui-item>
+    </ui-list>
+  </ui-list-group>
 </template>
 
 <script>
